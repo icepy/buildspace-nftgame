4ba2444f5df5d9548594bbe570a91151f648c80d
icepy <xiangwenwe@foxmail.com>
2022-01-02 00:18:35 +0800

    chore: 添加游戏:

---
+++ client/src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xa60E9Fe14F7EC18B6E760c0fd3821aF638D8d477';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
